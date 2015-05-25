3337c318e8b08fe30e487398b80d839151ba29e4
Feng Chen <VonChenPlus@gmail.com>
2015-05-25 14:09:23 +0800

    Update build script

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,13 +4,12 @@ set -e
 set -x
 
 #if OS is linux or is not set
-if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
+if [ "$CXX" = "g++" ]; then
     cd QT
     qmake -qt=qt5 -v
     qmake -qt=qt5 -r HiveQt.pro
     make
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    export Qt5_DIR=$(brew --prefix)/opt/qt5
+elif [ "$CXX" = "clang++" ]; then
     cd QT
     qmake -qt=qt5 -v
     qmake -makefile HiveQt.pro -spec macx-llvm
--- .travis-deps.sh
+++ .travis-deps.sh
@@ -4,14 +4,20 @@ set -e
 set -x
 
 #if OS is linux or is not set
-if [ "$TRAVIS_OS_NAME" = "linux" -o -z "$TRAVIS_OS_NAME" ]; then
+if [ "$CXX" = "g++" ]; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
     sudo apt-get -qq update
     sudo apt-get -qq install qt5-qmake qtbase5-dev qtdeclarative5-dev
     sudo apt-get -qq install g++-4.9 xorg-dev libglu1-mesa-dev libxcursor-dev
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    brew tap homebrew/versions
-    brew install qt5 glfw3 pkgconfig
+elif [ "$CXX" = "clang++" ]; then
+    cwd=$(pwd)
+    svn co --quiet http://llvm.org/svn/llvm-project/libcxx/trunk@201601 libcxx
+    cd libcxx/lib && bash buildit
+    sudo cp ./libc++.so.1.0 /usr/lib/
+    sudo mkdir /usr/include/c++/v1
+    cd .. && sudo cp -r include/* /usr/include/c++/v1/
+    cd /usr/lib && sudo ln -sf libc++.so.1.0 libc++.so
+    sudo ln -sf libc++.so.1.0 libc++.so.1 && cd $cwd
 fi
