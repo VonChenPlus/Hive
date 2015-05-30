a9c3556490882c681fcf28d097381935ca5e04ec
VonChenPlus <VonChenPlus@gmail.com>
2015-05-30 14:39:48 +0800

    update triavis scripts

--- .travis-build.sh
+++ .travis-build.sh
@@ -4,15 +4,9 @@ set -e
 set -x
 
 #if OS is linux or is not set
-if [ "$CXX" = "g++" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     cd QT
     qmake -qt=qt5 -v
     qmake -qt=qt5 -r HiveQt.pro
-    make
-elif [ "$CXX" = "clang++" ]; then
-    cd QT
-    QMAKE_MACOSX_DEPLOYMENT_TARGET = 
-    qmake -qt=qt5 -v
-    qmake -qt=qt5 -makefile HiveQt.pro -spec macx-clang
     make
 fi
--- .travis-deps.sh
+++ .travis-deps.sh
@@ -4,17 +4,11 @@ set -e
 set -x
 
 #if OS is linux or is not set
-if [ "$CXX" = "g++" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
     sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
     sudo apt-get -qq update
     sudo apt-get -qq install qt5-qmake qtbase5-dev qtdeclarative5-dev
     sudo apt-get -qq install g++-4.9 xorg-dev libglu1-mesa-dev libxcursor-dev
     sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90
-elif [ "$CXX" = "clang++" ]; then
-    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
-    sudo apt-get -qq update
-    sudo apt-get -qq install qt5-qmake qtbase5-dev qtdeclarative5-dev
-    sudo apt-get install -y llvm-3.4 llvm-3.4-dev
 fi
