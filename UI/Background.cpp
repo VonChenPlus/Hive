22cea606409aad4866ee333550031ae49ddedb39
unknown <VonChenPlus@gmail.com>
2015-11-17 22:51:51 +0800

    fixed build warnings

--- UI/Background.cpp
+++ UI/Background.cpp
@@ -31,10 +31,10 @@ bool Background::init() {
     GRAPH::Color4B color = 0xFFFFFFFF;
     GRAPH::V3F_C4B_T2F_Quad quads =
     {
-        { MATH::Vector3f(origin.x, origin.y, 0), 0xFFFFFFFF, GRAPH::Tex2F(getUIAtlas()[I_BG].u1, getUIAtlas()[I_BG].v2), },
-        { MATH::Vector3f(origin.x, origin.y + visibleSize.height, 0), 0xFFFFFFFF, GRAPH::Tex2F(getUIAtlas()[I_BG].u1, getUIAtlas()[I_BG].v1), },
-        { MATH::Vector3f(origin.x + visibleSize.width, origin.y, 0), 0xFFFFFFFF, GRAPH::Tex2F(getUIAtlas()[I_BG].u2, getUIAtlas()[I_BG].v2), },
-        { MATH::Vector3f(origin.x + visibleSize.width, origin.y + visibleSize.height, 0), 0xFFFFFFFF, GRAPH::Tex2F(getUIAtlas()[I_BG].u2, getUIAtlas()[I_BG].v1), },
+        { MATH::Vector3f(origin.x, origin.y, 0), color, GRAPH::Tex2F(getUIAtlas()[I_BG].u1, getUIAtlas()[I_BG].v2), },
+        { MATH::Vector3f(origin.x, origin.y + visibleSize.height, 0), color, GRAPH::Tex2F(getUIAtlas()[I_BG].u1, getUIAtlas()[I_BG].v1), },
+        { MATH::Vector3f(origin.x + visibleSize.width, origin.y, 0), color, GRAPH::Tex2F(getUIAtlas()[I_BG].u2, getUIAtlas()[I_BG].v2), },
+        { MATH::Vector3f(origin.x + visibleSize.width, origin.y + visibleSize.height, 0), color, GRAPH::Tex2F(getUIAtlas()[I_BG].u2, getUIAtlas()[I_BG].v1), },
     };
 
     uiAtlas_->updateQuad(&quads, 0);
@@ -97,14 +97,14 @@ void Background::update(float) {
         float y = ybase[i] + origin.y + 40 * cosf(i * 7.2f + curTime * 1.3f);
         float angle = sinf(i + curTime);
         int n = i & 3;
-        float width = (float) getUIAtlas()[symbols[n]].w * 1.2;
-        float height = (float) getUIAtlas()[symbols[n]].h * 1.2;
+        float width = (float) getUIAtlas()[symbols[n]].w * 1.2f;
+        float height = (float) getUIAtlas()[symbols[n]].h * 1.2f;
         GRAPH::Color4B color = GRAPH::Color4B::ColorAlpha(colors[n], 0.1f);
         float _v[4][2] = {
-            { x - width * 0.5, y - height * 0.5 },
-            { x - width * 0.5, y + height * 0.5 },
-            { x + width * 0.5, y - height * 0.5 },
-            { x + width * 0.5, y + height * 0.5 }, };
+            { x - width * 0.5f, y - height * 0.5f },
+            { x - width * 0.5f, y + height * 0.5f },
+            { x + width * 0.5f, y - height * 0.5f },
+            { x + width * 0.5f, y + height * 0.5f }, };
         for (auto &v : _v) {
             Rotated(v, angle, x, y);
         }
